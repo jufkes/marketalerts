3641f2b5de4e2d72b501d0fbe2f9356c3408a473
Chase Ufkes <jufkes@gmail.com>
2021-01-09 20:07:28 -0600

    made notes

--- src/main/java/io/crypto/marketalerts/service/CalculationService.java
+++ src/main/java/io/crypto/marketalerts/service/CalculationService.java
@@ -15,6 +15,14 @@ public class CalculationService {
     private final BinanceService binanceService;
     private final TokenRecord4hRepository tokenRecord4hRepository;
 
+    /* symbol = coinpair to retrieve. Eventually this will be a list of things to work against
+    for now, use ETHUSDT
+
+     intervals = 15m, 30m, 1h, 2h, 4h, 12h, 1d, 1w, 1mo
+     - need to retrieve market data on these intervals on a schedule. Then save their data to
+     collections correlated to them. So for 15m data, it would save to the a tokenRecord15m etc etc
+
+     period = the number of candles to retrieve. Could probably hard code this to 30 for now */
     public void processData(String symbol, String interval, Integer period) {
         List<CandleStickData> candleStickData = binanceService.getCandlesStickData(symbol, interval, period);
         RsiData rsi = TechnicalIndicatorHelper.calculateRsiData(candleStickData);
